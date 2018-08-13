74243f90f15f09fcfc0cf89ebaf8504e88a20160
Feliproo <felipeproencadeoliveira@gmail.com>
2018-08-13 20:01:26 -0300

    q

---
+++ exemplo1.php
@@ -0,0 +1,13 @@
+<!doctype <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>exemplo 1 PHP</title>
+    
+</head>
+<body>
+    <?php
+        echo "falando de <strong>ovo</strong>";
+    ?>
+</body>
+</html>
